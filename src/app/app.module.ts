cb767f3ea386bc44a42e3bf91c8bbb48e6d32204
Kezia Wineberg <Ksan8@protonmail.com>
2021-03-26 11:48:21 -0700

    Add Quill & configure editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { QuillModule } from 'ngx-quill';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot({
+      modules: {
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ 'header': 1 }, { 'header': 2 }],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'indent': '-1'}, { 'indent': '+1' }],
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        ],
+      },
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
